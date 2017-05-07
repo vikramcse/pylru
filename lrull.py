e9aa30dcd23366d27ed2b29139c24c474ec8f590
Vikram Jadhav <vikramcse.10@gmail.com>
2017-05-07 20:17:25 +0530

    added mutable dict interface to existing implementaion

--- lrull.py
+++ lrull.py
@@ -1,3 +1,6 @@
+from collections import MutableMapping
+
+
 class Node:
     def __init__(self, key, value):
         self.key = key
@@ -6,12 +9,50 @@ class Node:
         self.next = None
 
 
-class LRUCache:
+class LRUCache(MutableMapping):
     def __init__(self, capacity):
         self.capacity = capacity
         self.cache = {}
         self.head = None
         self.rear = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def set(self, key, value):
+        if key in self.cache:
+            old_node = self.cache.get(key)
+            old_node.value = value
+            self.remove_node(old_node)
+            self.push_front(old_node)
+        else:
+            new_node = Node(key, value)
+            if len(self.cache) >= self.capacity:
+                self.cache.pop(self.rear.key)
+                self.remove_node(self.rear)
+                self.push_front(new_node)
+            else:
+                self.push_front(new_node)
+
+            self.cache[key] = new_node
+            self.size += 1
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def get(self, key, default=None):
+        if key in self.cache:
+            temp_node = self.cache.get(key)
+            self.remove_node(temp_node)
+            self.push_front(temp_node)
+            return temp_node.value
+        elif default is not None:
+            return default
+        return -1
+
+    def __getitem__(self, key):
+        self.get(key, default=None)
 
     def push_front(self, node):
         if isinstance(node, Node):
@@ -43,32 +84,17 @@ class LRUCache:
             node.previous.next = node.next
             node.next.previous = node.previous
 
-    def set(self, key, value):
-        if key in self.cache:
-            old_node = self.cache.get(key)
-            old_node.value = value
-            self.remove_node(old_node)
-            self.push_front(old_node)
-        else:
-            new_node = Node(key, value)
-            if len(self.cache) >= self.capacity:
-                self.cache.pop(self.rear.key)
-                self.remove_node(self.rear)
-                self.push_front(new_node)
-            else:
-                self.push_front(new_node)
+    def __iter__(self):
+        pass
 
-            self.cache[key] = new_node
+    def __delitem__(self, key):
+        pass
 
-    def get(self, key):
-        if key in self.cache:
-            temp_node = self.cache.get(key)
-            self.remove_node(temp_node)
-            self.push_front(temp_node)
-            return temp_node.value
-        return -1
 
 cache = LRUCache(capacity=3)
-cache.set(1, "A")
-cache.set(2, "B")
-cache.set(3, "C")
+cache[1] = 'A'
+cache[2] = 'B'
+cache[4] = 'C'
+
+print cache[1]
+print len(cache)
